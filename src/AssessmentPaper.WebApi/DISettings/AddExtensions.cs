fdcc57691aa1ed030631a1dd0dc4fc42c758d8d7
NaveedAhmadHematmal <NaveedAhmadHematmal@gmail.com>
2021-12-20 11:34:04 +0430

    moves Identity services

--- src/AssessmentPaper.WebApi/DISettings/AddExtensions.cs
+++ src/AssessmentPaper.WebApi/DISettings/AddExtensions.cs
@@ -1,5 +1,11 @@
+using AssessmentPaper.WebApi.Areas.Data;
+using AssessmentPaper.WebApi.Areas.Policies.Admin;
+using AssessmentPaper.WebApi.Areas.Policies.Read;
+using AssessmentPaper.WebApi.Areas.Policies.Write;
 using AssessmentPaper.WebApi.Persistence;
 using AssessmentPaper.WebApi.Persistence.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace AssessmentPaper.WebApi.DISettings;
 
@@ -11,6 +17,50 @@ public static class AddExtensions
         services.AddSingleton<QuestionRepository>();
         services.AddSingleton<TagRepository>();
         services.AddSingleton<CategoryRepository>();
+        
+        return services;
+    }
+
+    public static IServiceCollection AddIdentity(this IServiceCollection services)
+    {
+        // adds Identity to WebApi
+        services.AddIdentity<IdentityUser, IdentityRole>(options => 
+            options.Password.RequiredLength = 8)
+            .AddEntityFrameworkStores<IdentityDatabaseContext>()
+            .AddDefaultTokenProviders();
+
+        // Authorization Policies
+        services.AddAuthorization(options =>{
+            options.AddPolicy(
+                "CanWrite",
+                policyBuilder => 
+                policyBuilder.AddRequirements(
+                    new WriteRequirement()
+                )
+            );
+            options.AddPolicy(
+                "CanRead",
+                policyBuilder =>
+                policyBuilder.AddRequirements(
+                    new ReadRequirement()
+                )
+            );
+            options.AddPolicy(
+                "CanControl",
+                policyBuilder =>
+                policyBuilder.AddRequirements(
+                    new AdminRequirement()
+                )
+            );
+        });
+
+        // Authorization Handlers
+        services.AddSingleton<IAuthorizationHandler, AdminHandler>();
+        services.AddSingleton<IAuthorizationHandler, WriteHandler>();
+        services.AddSingleton<IAuthorizationHandler, ReadHandler>();
+        services.AddSingleton<IAuthorizationHandler, AdminToWriteHandler>();
+        services.AddSingleton<IAuthorizationHandler, AdminToReadHandler>();
+        services.AddSingleton<IAuthorizationHandler, WriteToReadHandler>();
         
         return services;
     }
